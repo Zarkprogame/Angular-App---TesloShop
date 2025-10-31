129526ccfe21c36312102aabc0405ae628789158
Zarkprogame <zarkprogame@gmail.com>
2025-10-31 05:30:11 -0500

    Levantamos el Backend y mostramos Productos

--- src/app/products/product-card/product-card.ts
+++ src/app/products/product-card/product-card.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { SlicePipe } from '@angular/common';
+import { Component, input } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { Product } from '@products/interfaces/product.interface';
+import { ProductImagePipe } from '../pipes/product-image.pipe';
 
 @Component({
   selector: 'product-card',
-  imports: [RouterLink],
+  imports: [RouterLink, SlicePipe, ProductImagePipe],
   templateUrl: './product-card.html',
 })
-export class ProductCard { }
+export class ProductCard {
+  product = input.required<Product>();
+}
--- src/app/store-front/pages/home-page/home-page.component.ts
+++ src/app/store-front/pages/home-page/home-page.component.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { ProductCard } from '@products/product-card/product-card';
+import { ProductService } from '@products/services/products.service';
+import { rxResource } from '@angular/core/rxjs-interop'
 
 @Component({
   selector: 'app-home-page',
   imports: [ProductCard],
   templateUrl: './home-page.component.html',
 })
-export class HomePageComponent { }
+export class HomePageComponent {
+  productService = inject(ProductService);
+
+  productsResource = rxResource({
+    params: () => ({}),
+    stream: ({ params }) => {
+      return this.productService.getProducts({});
+    }
+  })
+}
+
